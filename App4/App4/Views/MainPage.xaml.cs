b75aaaa11f0394c8e0bfbf04553614f2d713e591
B00zer0 <74270856+B00zer0@users.noreply.github.com>
2023-01-18 16:19:05 +0300

    Create db (#5)
    * Улучшил визуал и добавил кнопки
    
    * f
    
    * .
    
    * .
    
    * Fixed
    
    * gbplf
    
    * fa
    
    * Squashed commit of the following:
    
    commit 0b4d0311759b5a5c2f598185d60625d09c15a631
    Author: B00zer0 <74270856+B00zer0@users.noreply.github.com>
    Date:   Wed Jan 18 15:45:11 2023 +0300
    
        xz (#6)
    
    commit b9d288b7250020d3133170b0ffb8a59cce719ff7
    Author: B00zer0 <Artur.krasickov@yandex.ru>
    Date:   Wed Jan 18 15:28:14 2023 +0300
    
        d
    
        d
    
    commit e95ead5e56a7a807f6e58842d23b61f7ff06cb0b
    Author: B00zer0 <Artur.krasickov@yandex.ru>
    Date:   Wed Jan 18 15:27:54 2023 +0300
    
        d
    
        d
    
    commit 75f7e14f0be21257436a8514ce65a4a1d1ba3b5f
    Author: B00zer0 <74270856+B00zer0@users.noreply.github.com>
    Date:   Tue Jan 17 21:59:59 2023 +0300
    
        Улучшил визуал и добавил кнопки (#2)
    
    * test
    
    * g
    
    g

---
+++ App4/App4/Views/MainPage.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using Xamarin.Forms;
+using static Xamarin.Essentials.Permissions;
+
+namespace App4
+{
+    public partial class MainPage : ContentPage
+    {
+        
+        private double container;
+        private double mass;
+        private int numOfContainers;
+        private string result;
+        
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+        
+
+        private void BttResult_Clicked(object sender, EventArgs e)
+        {
+            ContainerContentCounting();
+            result_output.Text = result;
+        }
+
+    private void ContainerContentCounting()
+        {
+            container = Convert.ToDouble(ContainerB.Text);
+            mass = Convert.ToDouble(Mass.Text);
+            numOfContainers = Convert.ToInt32(numofcontainers.Text);
+            result = Convert.ToString(mass - container * numOfContainers);
+            
+
+        }
+
+        private void BtnPlus_Clicked(object sender, EventArgs e)
+        {
+            
+            if(result_output.Text == "0")
+            {
+                ContainerContentCounting();
+                result_output.Text = result;
+            }
+            else
+            {
+                ContainerContentCounting();
+                result_output.Text = Convert.ToString(Convert.ToDouble(result_output.Text) + Convert.ToDouble(result));
+            }
+        }
+
+        private void BtnClr_Clicked(object sender, EventArgs e)
+        {
+            result_output.Text = "0";
+        }
+    }
+}
