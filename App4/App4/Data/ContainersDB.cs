c82e9c8f1d99822b34d5e872e98bcf2b1512a66c
B00zer0 <Artur.krasickov@yandex.ru>
2023-01-18 03:01:29 +0300

    f

---
+++ App4/App4/Data/ContainersDB.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections.Generic;
+using SQLite;
+using App4.Models;
+using System.Threading.Tasks;
+
+namespace App4.Data
+{
+    class ContainersDB
+    {
+        readonly SQLiteAsyncConnection db;
+        public ContainersDB(string connectionString)
+        {
+            db = new SQLiteAsyncConnection(connectionString);
+
+            db.CreateTableAsync<Container>().Wait();
+        }
+
+        public Task<List<Container>> GetContainersAsync()
+        {
+            return db.Table<Container>().ToListAsync();
+        }
+
+        public Task<Container> GetContainerAsync(int id)
+        {
+            return db.Table<Container>().Where(i => i.ID== id).FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveContainerAsync(Container container)
+        {
+            if(container.ID != 0)
+            {
+                return db.UpdateAsync(container);
+            }
+            else
+            {
+                return db.InsertAsync(container);
+            }
+            
+        }
+
+        public Task<int> DeleteContainerAsync(Container container)
+        {
+            return db.DeleteAsync(container);
+        }
+    }
+}
